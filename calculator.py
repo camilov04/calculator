7be62bf1a92bdfc44660fa0081684bf1f10e1f8a
Camilo Velasquez <velasquezhurtadoc@gmail.com>
2021-08-06 15:27:54 -0500

    se creo calculadora de restas

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def resta ():
+    numero1 = float (input ('ingrese el primer numero: '))
+    numero2 = float (input ('ingrese el segundo numero: '))
+
+    resultado=numero1-numero2
+    print("la resta de", numero1, "por", numero2, "es: ", resultado)
+
+resta()
